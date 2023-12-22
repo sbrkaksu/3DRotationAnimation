e6a089f8fdcf521613bc48218a07b512798e8b94
Berk Aksu <berkaksu79@gmail.com>
2023-12-23 02:46:16 +0300

    code was revised

--- ThreeDRotationAnimation/ContentView.swift
+++ ThreeDRotationAnimation/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animationAmount = 0.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button("Tap Me"){
+                withAnimation(.easeIn(duration: 1.5)){
+                    animationAmount += 360
+                }
+            }
+            .padding(50)
+            .foregroundColor(.white)
+            .background(.cyan)
+            .clipShape(Circle())
+            .rotation3DEffect(.degrees(animationAmount), axis: (x:0, y:1, z:0))
         }
-        .padding()
     }
 }
 
