3b2b0d03a807c1e69fd013572367ddb1eba379f6
Berk Aksu <berkaksu79@gmail.com>
2023-12-23 02:18:58 +0300

    Initial Commit

---
+++ ThreeDRotationAnimation/ThreeDRotationAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreeDRotationAnimationApp.swift
+//  ThreeDRotationAnimation
+//
+//  Created by Berk Aksu on 23.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreeDRotationAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
